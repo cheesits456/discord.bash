efea84a4d9f4b32e732d07314c0ded7d239398e3
Quin <quin@cheesits456.dev>
2023-02-27 21:53:49 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
-repo="https://github.com/cheesits456/discord.bash"
-version="1.0.0"
-api_version="10"
+#!/bin/bash
+export repo="https://github.com/cheesits456/discord.bash"
+export version="1.0.0"
+export api_version="10"
