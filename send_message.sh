dd7b604f4b6db47498ca34c4424ea2f939f09ed2
Quin <quin@cheesits456.dev>
2023-02-27 21:40:24 -0800

    Update send_message.sh

--- send_message.sh
+++ send_message.sh
@@ -4,9 +4,9 @@ source ./config.sh
 
 # Usage: ./send_message.sh <channel_id> <message_content>
 
-curl -X POST \
-     -A "discord.bash ($repo, $version)" \
-     -H "Authorization: Bot $TOKEN" \
-     -H "Content-Type: application/json"\
-     -d "{\"content\": \"$2\"}" \
-     https://discord.com/api/v$api_version/channels/$1/messages
+curl	-X POST \
+		-A "discord.bash ($repo, $version)" \
+		-H "Authorization: Bot $TOKEN" \
+		-H "Content-Type: application/json"\
+		-d "{\"content\": \"$2\"}" \
+		https://discord.com/api/v$api_version/channels/$1/messages
