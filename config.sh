eee88d69584650104e95e9716c9ccc6541e606e9
Quin <quin@cheesits456.dev>
2023-02-28 22:52:39 -0800

    Update shebangs

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 export repo="https://github.com/cheesits456/discord.bash"
 export version="1.0.0"
 export api_version="10"
--- messages/edit.sh
+++ messages/edit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 source ./.env
 source ./config.sh
 
