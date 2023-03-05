0ca5e84b8f26143049305f5338cfbedd6fb1e31e
Quin <quin@cheesits456.dev>
2023-03-05 11:43:43 -0800

    Update code formatting

--- index.sh
+++ index.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 source "$SCRIPT_DIR/.env"
 source "$SCRIPT_DIR/config.sh"
