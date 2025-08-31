18666a5c1bc348f339ad750819dd5318e64e80b2
Quin F. <quin@cheesits456.dev>
2025-08-31 09:37:33 -0700

    Add comments

--- index.sh
+++ index.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
+
+# Set variable for root directory path
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+# Source necessary scripts
 source "$SCRIPT_DIR/.env"
 source "$SCRIPT_DIR/config.sh"
 source "$SCRIPT_DIR/json-parse.sh"
 
+# Source API functions
 for file in "$SCRIPT_DIR"/*/*; do
 	source "$file"
 done
 
+# Function to echo to stderr
 errcho() { echo >&2 "$@"; }
 
+# Function to print a pretty and useful error message
 # args: $1 caller, $2 message
 error_message() {
 	function=$(caller | tac -s/ | head -n1 | awk -F. '{print $1}')
@@ -23,12 +29,14 @@ error_message() {
 	)"
 }
 
+# Helper function to generate a random file name
 generate_filename() {
 	for i in {1..8}; do
 		printf "%x" $((RANDOM % 15))
 	done
 }
 
+# Function to parse JSON data returned by API
 parse_json() {
 	filename=$(generate_filename)
 	echo -n "$1" > "$filename"
